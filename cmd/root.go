628162dbbd49fc20e8d3061b1b0dc544abf714b2
Lars Wegmann <mail@larswegmann.de>
2022-11-09 15:20:40 +0100

    Add help message

--- cmd/root.go
+++ cmd/root.go
@@ -27,18 +27,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "impose",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Image version updater for Docker Compose",
+	Long: `Image version updater for Docker Compose.
+This tool automatically scans the given Docker Compose
+file for image versions and updates them.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Example:
+./impose update`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -62,7 +60,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
